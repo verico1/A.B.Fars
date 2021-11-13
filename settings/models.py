c044d28a22119017583c741629054c2dd0bbe16e
verico1 <mehranbahrami281@gmail.com>
2021-11-13 14:12:37 +0330

    another change

--- settings/models.py
+++ settings/models.py
@@ -8,10 +8,16 @@ class Setting(models.Model):
     title = TranslatedField(models.CharField(max_length=120, blank=True, null=True))
     about_us = TranslatedField(models.CharField(max_length=800, blank=True, null=True))
     about_us_small = TranslatedField(models.CharField(max_length=600, blank=True, null=True))
+    
     instagram = models.CharField(max_length=120)
     whatsapp = models.CharField(max_length=120)
     telegram =models.CharField(max_length=120)
     email = models.EmailField()
+
+    products_count = models.PositiveIntegerField(default=0)
+    international_customers = models.PositiveIntegerField(default=0)
+    years_of_activity = models.PositiveIntegerField(default=0)
+    domestic_customers = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return 'تنظیمات'
