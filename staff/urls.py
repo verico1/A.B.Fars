019db14b0ec3ace61bcdc9bb81bb67d35d91324d
verico1 <mehranbahrami281@gmail.com>
2021-11-15 13:57:25 +0330

    dashboard templates cleaned by files

--- staff/urls.py
+++ staff/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name="staff"
 urlpatterns = [
     path('', views.staff_index, name="staff_index"),
+    path('messages/', views.staff_messages, name="staff_messages"),
+    path('categories/', views.staff_categories, name="staff_categories"),
+    path('products/', views.staff_products, name="staff_products"),
+    path('create-product/', views.staff_create_products, name="staff_create_products"),
+    path('settings/', views.staff_settings, name="staff_settings"),
 ]
--- staff/views.py
+++ staff/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def staff_index(request):
     return render(request, "staff/home.html")
+
+def staff_messages(request):
+    return render(request, "staff/messages.html")    
+
+def staff_categories(request):
+    return render(request, "staff/categories.html")        
+
+def staff_products(request):
+    return render(request, "staff/products.html")  
+
+def staff_create_products(request):
+    return render(request, "staff/create_product.html")     
+
+def staff_settings(request):
+    return render(request, "staff/settings/settings.html")
