3beb76411479852088f3b5ac9ae2d95127548f6d
verico1 <mehranbahrami281@gmail.com>
2021-11-28 22:31:56 +0330

    db updated for settings

--- main/context_processors.py
+++ main/context_processors.py
@@ -4,8 +4,7 @@ import random
 
 def setting(requet, **kwargs):
     setting = Setting.objects.first()
-    products = Product.objects.all()
-    random_products = random.sample(list(products), 3)
+    random_products = Product.objects.all()
     ctx = {'setting':setting,
         'random_products':random_products,
         }
