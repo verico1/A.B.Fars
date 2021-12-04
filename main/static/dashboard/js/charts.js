9f590a08b10e0c5bf4793c5b91a18588fff22f1d
Hamid <hmotagli@gmail.com>
2021-12-04 16:21:26 +0330

    edited chart from the frontend

--- main/static/dashboard/js/charts.js
+++ main/static/dashboard/js/charts.js
@@ -1,3 +1,4 @@
+const view_data = document.getElementById('view_data').innerHTML.split('[')[1].split(']')[0].split(',');
 const ctx1 = document.getElementById('chartViewPage').getContext('2d');
 const chartViewPage = new Chart(ctx1, {
     type: 'line',
@@ -8,7 +9,7 @@ const chartViewPage = new Chart(ctx1, {
         labels: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'],
         datasets: [{
             label: 'بازدید',
-            data: [40, 53, 43, 52, 100, 95, 130, 145],
+            data: view_data,
             backgroundColor: [
                 '#FF2626'
             ],
