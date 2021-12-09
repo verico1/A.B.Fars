878ebaaa837c585b802cc03675c85edd21bbf36c
Hamid <hmotagli@gmail.com>
2021-12-09 13:40:42 +0330

    added loading mode

---
+++ main/static/js/loading.js
@@ -0,0 +1,15 @@
+function disable_loading() {
+    const loading = document.getElementById('loading');
+    setTimeout(() => {
+        loading.classList.add('fade')
+        document.body.style.overflowY = "auto";
+        setTimeout(() => {
+            loading.classList.add('d-none')
+        }, 500);
+    }, 300);
+}
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', disable_loading);
+} else {
+    disable_loading();
+}
